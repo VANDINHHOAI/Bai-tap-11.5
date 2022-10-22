c655018f4d39839acc1a38b9f181be36a7c5c5e8
VANDINHHOAI <115123521+VANDINHHOAI@users.noreply.github.com>
2022-10-22 19:32:32 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,16 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 100;
+		int b = 999;
+		while (a <= b) {
+			if (a % 3 == 0) {
+				System.out.println(a);
+			}
+			a++;
+		}
+	}
+}
